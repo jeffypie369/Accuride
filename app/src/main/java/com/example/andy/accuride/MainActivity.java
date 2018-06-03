0e01e3a0579f9e2e50ff812f966563298aaaa8c3
DESKTOP-4O94ROD\Andy <andychan1451@gmail.com>
2018-06-03 16:01:55 +0800

    Add Search Bar

--- app/src/main/java/com/example/andy/accuride/MainActivity.java
+++ app/src/main/java/com/example/andy/accuride/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.andy.accuride;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Spinner;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> stationNames = new ArrayList<>();
+    AutoCompleteTextView suggestion_box;
+    Spinner stationsSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initStations();
+
+        suggestion_box = findViewById(R.id.suggestion_box);
+        stationsSpinner = findViewById(R.id.stations);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this,
+                android.R.layout.simple_spinner_dropdown_item, stationNames);
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(MainActivity.this,
+                android.R.layout.simple_spinner_dropdown_item, stationNames);
+
+        suggestion_box.setAdapter(adapter);
+
+        stationsSpinner.setAdapter(adapter2);
+    }
+
+    private void initStations() {
+        stationNames.add("Pasir Ris");
+        stationNames.add("Tampines");
+        stationNames.add("Simei");
+        stationNames.add("Tanah Merah");
+        stationNames.add("Bedok");
+        stationNames.add("Kembangan");
+        stationNames.add("Eunos");
+        stationNames.add("Paya Lebar");
+        stationNames.add("Aljunied");
+        stationNames.add("Kallang");
+        stationNames.add("Lavender");
+        stationNames.add("Bugis");
     }
 }
